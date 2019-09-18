feaa96c25859c79c5d2d24988cfca76e7fafd527
Nick Ager <nick.ager@gmail.com>
2019-09-18 12:47:45 +0100

    temporary HUD hack to fix `Main Thread Checker` error * see pkluz/PKHUD#245 (with my comment) * see pkluz/PKHUD#251 - which documents an attempted fix in pull request pkluz/PKHUD/#252 * see another attempt which just conditionally enables the code - pkluz/PKHUD/#260

--- PKHUD/FrameView.swift
+++ PKHUD/FrameView.swift
@@ -33,20 +33,20 @@ internal class FrameView: UIVisualEffectView {
 
         contentView.addSubview(content)
 
-        let offset = 20.0
-
-        let motionEffectsX = UIInterpolatingMotionEffect(keyPath: "center.x", type: .tiltAlongHorizontalAxis)
-        motionEffectsX.maximumRelativeValue = offset
-        motionEffectsX.minimumRelativeValue = -offset
-
-        let motionEffectsY = UIInterpolatingMotionEffect(keyPath: "center.y", type: .tiltAlongVerticalAxis)
-        motionEffectsY.maximumRelativeValue = offset
-        motionEffectsY.minimumRelativeValue = -offset
-
-        let group = UIMotionEffectGroup()
-        group.motionEffects = [motionEffectsX, motionEffectsY]
-
-        addMotionEffect(group)
+//        let offset = 20.0
+//
+//        let motionEffectsX = UIInterpolatingMotionEffect(keyPath: "center.x", type: .tiltAlongHorizontalAxis)
+//        motionEffectsX.maximumRelativeValue = offset
+//        motionEffectsX.minimumRelativeValue = -offset
+//
+//        let motionEffectsY = UIInterpolatingMotionEffect(keyPath: "center.y", type: .tiltAlongVerticalAxis)
+//        motionEffectsY.maximumRelativeValue = offset
+//        motionEffectsY.minimumRelativeValue = -offset
+//
+//        let group = UIMotionEffectGroup()
+//        group.motionEffects = [motionEffectsX, motionEffectsY]
+//
+//        addMotionEffect(group)
     }
 
     private var _content = UIView()
